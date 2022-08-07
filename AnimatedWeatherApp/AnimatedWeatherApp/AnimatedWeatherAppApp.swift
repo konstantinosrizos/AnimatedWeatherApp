abc1472a1e5388d1e40e20db5acea21e1f7676df
konstantinosrizos <rizos.dev@gmail.com>
2022-08-07 12:35:46 +0100

    Setting up the project

---
+++ AnimatedWeatherApp/AnimatedWeatherApp/AnimatedWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedWeatherAppApp.swift
+//  AnimatedWeatherApp
+//
+//  Created by Konstantinos Rizos on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedWeatherApp/AnimatedWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatedWeatherApp
+//
+//  Created by Konstantinos Rizos on 7/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
